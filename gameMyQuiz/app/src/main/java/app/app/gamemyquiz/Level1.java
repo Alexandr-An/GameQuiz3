29986e20d79d08a912b46e9dfb2b0e2b84417a9c
AlAndreev92 <brabusmax@mail.ru>
2020-03-24 16:03:46 +0300

    level добавил карточки

--- gameMyQuiz/app/src/main/java/app/app/gamemyquiz/Level1.java
+++ gameMyQuiz/app/src/main/java/app/app/gamemyquiz/Level1.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -12,6 +13,14 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        final ImageView img_left= (ImageView)findViewById(R.id.img_left);
+        //код который скругляет углы
+        img_left.setClipToOutline(true);
+        final ImageView img_right= (ImageView)findViewById(R.id.img_right);
+        //код который скругляет углы
+        img_right.setClipToOutline(true);
+
         //развернуть инру на весь экран начало
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
