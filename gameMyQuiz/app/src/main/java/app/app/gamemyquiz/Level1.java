d7099f02ea22ae6d8581faed2d7c5958cd0fc044
AlAndreev92 <brabusmax@mail.ru>
2020-03-25 13:07:46 +0300

    диалоговое окно 1level

--- gameMyQuiz/app/src/main/java/app/app/gamemyquiz/Level1.java
+++ gameMyQuiz/app/src/main/java/app/app/gamemyquiz/Level1.java
@@ -2,12 +2,17 @@ package app.app.gamemyquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.CollapsibleActionView;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +30,13 @@ public class Level1 extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         //развернуть инру на весь экран конец
+
+        //вызов диалогового окна в начале инры
+        dialog = new Dialog(this);//пример диалогового окна
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//скрыть заголовок
+        dialog.setContentView(R.layout.previewdialog);// путь к макету диалогового окна
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));// прозрачность диалогового окна
+        dialog.setCancelable(false);// окно нельзя закрыть кнопкой назад
+        dialog.show();//показать диалоговое окно
     }
 }
